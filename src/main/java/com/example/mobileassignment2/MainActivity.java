aa3433ec7b9b351405943c40a25c446bdcee25ca
youseuf2104 <youseuf.molla@ontariotechu.net>
2021-11-17 23:43:21 -0500

    adding comments

--- src/main/java/com/example/mobileassignment2/MainActivity.java
+++ src/main/java/com/example/mobileassignment2/MainActivity.java
@@ -19,17 +19,16 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //Initialize the input texts and buttons  that need to be accessed later
     EditText  latitude;
     EditText longitude;
     EditText addressText;
-    Button searchButton,insertButton,checkButton,locationButton;
+    Button searchButton,insertButton,locationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         latitude = findViewById(R.id.latInsert);
         longitude = findViewById(R.id.longInsert);
@@ -37,21 +36,18 @@ public class MainActivity extends AppCompatActivity {
 
         searchButton =findViewById(R.id.searchButton );
         locationButton = findViewById(R.id.locationButton);
-        checkButton = findViewById(R.id.checkButton);
         insertButton = findViewById(R.id. insertButton);
 
 
         locationDatabase database = new locationDatabase(this);
-        this.deleteDatabase("location.db");
-        //database.insertLocation("second",323131.5253,23313.7747);
-        //String word = database.DATABASE_NAME;
+        //this.deleteDatabase("location.db");
 
-        //database.insertLocation("from on create",2,50);
+        //Insert the initial database values (addresses are generated using geocoding)
+        //before storing in database
         insertToDatabase(1.384127, 103.868290);
         insertToDatabase(48.858372, 2.294481);
         insertToDatabase( 43.7428, -79.20462);
         insertToDatabase( 43.90012, -78.84957);
-
         insertToDatabase( 51.90798, -8.59724);
         insertToDatabase( 51.90125, -8.60142);
         insertToDatabase( 38.9117,-77.01017 );
@@ -59,7 +55,8 @@ public class MainActivity extends AppCompatActivity {
         insertToDatabase(38.9117 , -77.01017);
         insertToDatabase( 38.91155, -77.01035);
 
-
+        //Search the database for the inputted Address, will display the latitude and longitudes
+        //using a toast message to the user
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,117 +73,53 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        //Use the inputted Address to generate appropriate coordinates using geocoding,
+        //then insert the values into the location table in the database
         insertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 locationDatabase database = new locationDatabase(MainActivity.this);
-                //double latNumber = Double.parseDouble(latitude.getEditableText().toString());
-                //String latNumber = latitude.getText().toString();
+
+                String theAddress = addressText.getText().toString();
 
                 List<Address> list;
                 Geocoder gcd = new Geocoder(MainActivity.this, Locale.getDefault());
                 try {
-                    list = gcd.getFromLocationName("1600 Amphitheatre Parkway, Mountain View, CA", 1);
-                    Address lat = list.get(0);
-                    String message = lat.toString();
-                    Toast myToast = Toast.makeText(MainActivity.this,"Success: "+message,Toast.LENGTH_SHORT);
-                    myToast.show();
-                    //database.insertLocation(lat.getAddressLine(0),);
+                    list = gcd.getFromLocationName(theAddress, 1);
+                    Address thisAddress= list.get(0);
+                    double latitude = thisAddress.getLatitude();
+                    double longitude = thisAddress.getLongitude();
+                    database.insertLocation(thisAddress.getAddressLine(0),latitude,longitude);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-                //findAddress(view);
-
-
             }
         });
 
-
-        checkButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String latNumber = latitude.getText().toString();
-                //double longNumber = Double.parseDouble(longitude.getText().toString());
-                Toast another = Toast.makeText(MainActivity.this,"Success: " +latNumber,Toast.LENGTH_SHORT);
-                another.show();
-                /*
-                double latNumber = Double.parseDouble(latitude.getText().toString());
-                double longNumber = Double.parseDouble(longitude.getText().toString());
-                lat = latitude.getText().toString();
-                //longi = longitude.getText().toString().trim();
-
-                Toast another = Toast.makeText(getActivity(),"Success: " +lat,Toast.LENGTH_SHORT);
-                another.show();
-
-                 */
-
-            }
-        });
-
-
+        //Test button to try inserting coordinates to the database
         locationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 locationDatabase database = new locationDatabase(MainActivity.this);
-                //getContext().deleteDatabase("location.db");
-                //database.insertLocation("second",323131.5253,23313.7747);
-                //String word = database.DATABASE_NAME;
-
-                database.insertLocation("example2",2,50);
-                //insertToDatabase(1.384127, 103.868290);
-                //insertToDatabase(48.858372, 2.294481);
                 insertToDatabase(43.7428, -79.20462);
-                /*
-                List<Address> list;
-                Geocoder gcd = new Geocoder(getContext(), Locale.getDefault());
-
-                try {
-                    //list = gcd.getFromLocation(1.384127, 103.868290, 1); //singapore
-                    list = gcd.getFromLocation(	48.858372, 2.294481, 1); //paris
-                    //list = gcd.getFromLocation(40.65833044834671, -73.99418920683213, 1);
-
-                    //String message = list.get(0).getAddressLine(0);  //one i need
-                    //String message = list.get(0).toString();
-
-                    //Toast myToast = Toast.makeText(getActivity(),"Success: "+message,Toast.LENGTH_LONG);
-                    //myToast.show();
-                    //database.insertLocation(message,48.858372, 2.294481);
-                    insertToDatabase(1.384127, 103.868290);
-                    //SystemClock.sleep(2000);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                */
-
 
             }
         });
     }
 
+    //Function that takes the coordinates and generates the address using geocoding
+    //then inserts the values into the location table in the database
     private void insertToDatabase(double latitude, double longitude){
         locationDatabase database = new locationDatabase(this);
-        //getContext().deleteDatabase("location.db");
-        //database.insertLocation("second",323131.5253,23313.7747);
-        //String word = database.DATABASE_NAME;
 
         List<Address> list;
         Geocoder gcd = new Geocoder(this, Locale.getDefault());
         try {
-            //list = gcd.getFromLocation(1.384127, 103.868290, 1); //singapore
-            //list = gcd.getFromLocation(	48.858372, 2.294481, 1); //paris
-            //list = gcd.getFromLocation(40.65833044834671, -73.99418920683213, 1);
             list = gcd.getFromLocation(latitude, longitude, 1);
             String address = list.get(0).getAddressLine(0);
-            //String message = list.get(0).toString();
-            //if (list.size()>0){
-            //findAddress();
+
             database.insertLocation(address,latitude, longitude);
-            //else{
-            //  Toast myToast = Toast.makeText(getActivity(),"Invalid Coordinates ",Toast.LENGTH_SHORT);
-            //myToast.show();
-            //}
-            //SystemClock.sleep(2000);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
